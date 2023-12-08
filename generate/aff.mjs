9a544ab9d75f19e8491b74c01ede8dccacf4e57f
Albert Mink <albert.mink@sap.com>
2023-12-08 14:15:54 +0100

    provide global line numbers

--- generate/aff.mjs
+++ generate/aff.mjs
@@ -10,6 +10,29 @@ async function run() {
     fs.mkdirSync("generated");
   }
 
+  function createAnnotations(diff, file) {
+    let lines = diff.split('\n');
+
+    let lineNumber;
+    for (let i = 0; i < lines.length; i++) {
+      const regExp = /^[0-9]/;
+
+      let line = lines[i];
+      if (line.startsWith('>')) {
+          let text = line.split(' ').slice(1).join(' ');
+          console.log(`::warning file=${file},line=${lineNumber}::${text}`);
+          ++lineNumber;
+      } else if (regExp.test(line)) {
+        lineNumber = parseInt(line.split("c")[0].split(",")[0]); // 16
+        //core.info(lineNumber);
+      } else if (line.startsWith("<")) {
+
+      }
+    }
+
+  }
+
+
   const types = [];
   for (const f of fs.readdirSync("../file-formats/")) {
     if (f.length === 4) {
@@ -28,34 +51,17 @@ async function run() {
 
     const result = await abap.Classes["CL_RUN"].run({object_type: new abap.types.String().set(type)});
     const filename = "generated" + path.sep + type.toLowerCase() + "-v1.json";
+    const filename_aff = `../file-formats/${type.toLowerCase()}/${type.toLowerCase()}-v1.json`;
     fs.writeFileSync(filename, result.get());
 
 
-    const command = `diff -u generated/${type.toLowerCase()}-v1.json ../file-formats/${type.toLowerCase()}/${type.toLowerCase()}-v1.json`;
+    const command = `diff ${filename} ${filename_aff}`;
     const output = child_process.execSync(`${command} || true`);
     if (output.toString().length > 0) {
       core.setFailed(type+": Provided and generated JSON Schema differ")
+      core.info(command);
       core.info(output.toString());
-      core.info("generated JSON Schema for " + type + "\n" + result.get());
-
-      let lines = output.toString().split('\n');
-
-      let currentFile;
-      let lineNumber;
-
-      lines.forEach(line => {
-        if (line.startsWith('+++')) { // Considering updated file only
-          let relativeFile = line.slice(4).split('\t')[0]; // Grab only filename, discard timestamp
-          currentFile = path.resolve(relativeFile);
-        }
-        else if (line.startsWith('@@')) {
-          lineNumber = parseInt(line.split('-')[1].split(',')[0]);
-        }
-        else if (line.startsWith('+')) {
-          // Create GitHub annotation
-          console.log(`::warning file=${currentFile},line=${lineNumber}::${line.slice(2)}`);
-        }
-      });
+      createAnnotations(output.toString(), path.resolve(filename_aff));
     }
 
   }
