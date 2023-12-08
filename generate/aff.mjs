90e739b2cdd7fed7abc536d6aa76805da6ca60e9
Albert Mink <albert.mink@sap.com>
2023-12-08 14:17:47 +0100

    break json schema

--- generate/aff.mjs
+++ generate/aff.mjs
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as child_process from 'node:child_process';
-import {initializeABAP} from "./output/init.mjs";
+import { initializeABAP } from "./output/init.mjs";
 await initializeABAP();
 import * as core from '@actions/core';
 
@@ -14,21 +14,18 @@ async function run() {
     let lines = diff.split('\n');
 
     let lineNumber;
-    for (let i = 0; i < lines.length; i++) {
-      const regExp = /^[0-9]/;
+    const regExp = /^[0-9]/;
 
-      let line = lines[i];
+    lines.forEach(line => {
+
       if (line.startsWith('>')) {
-          let text = line.split(' ').slice(1).join(' ');
-          console.log(`::warning file=${file},line=${lineNumber}::${text}`);
-          ++lineNumber;
+        let text = line.split(' ').slice(1).join(' ');
+        console.log(`::error file=${file},line=${lineNumber}::${text}`);
+        ++lineNumber;
       } else if (regExp.test(line)) {
         lineNumber = parseInt(line.split("c")[0].split(",")[0]); // 16
-        //core.info(lineNumber);
-      } else if (line.startsWith("<")) {
-
       }
-    }
+    });
 
   }
 
@@ -45,23 +42,25 @@ async function run() {
   let error = false;
   for (const type of types) {
     if (type === "ENHO") {
-      core.notice(type+" skipped, https://github.com/SAP/abap-file-formats/issues/409");
+      core.notice(type + " skipped, https://github.com/SAP/abap-file-formats/issues/409");
       continue;
     }
 
-    const result = await abap.Classes["CL_RUN"].run({object_type: new abap.types.String().set(type)});
+    const result = await abap.Classes["CL_RUN"].run({ object_type: new abap.types.String().set(type) });
     const filename = "generated" + path.sep + type.toLowerCase() + "-v1.json";
     const filename_aff = `../file-formats/${type.toLowerCase()}/${type.toLowerCase()}-v1.json`;
     fs.writeFileSync(filename, result.get());
 
 
-    const command = `diff ${filename} ${filename_aff}`;
+    const command = `diff ${filename_aff} ${filename}`;
     const output = child_process.execSync(`${command} || true`);
     if (output.toString().length > 0) {
-      core.setFailed(type+": Provided and generated JSON Schema differ")
-      core.info(command);
-      core.info(output.toString());
+      core.setFailed(type + ": Provided and generated JSON Schema differ")
       createAnnotations(output.toString(), path.resolve(filename_aff));
+      //core.info(command);
+      //core.info(output.toString());
+    } else {
+      core.notice(type + " success");
     }
 
   }
