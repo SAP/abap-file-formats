384b3ce3ca446ad56a1c9d5ced329bcc25881308
Albert Mink <albert.mink@sap.com>
2024-03-25 18:46:00 +0100

    one function

--- compatibility-check/json-schema-diff.js
+++ compatibility-check/json-schema-diff.js
@@ -4,7 +4,11 @@ const core = require('@actions/core');
 const {readFileSync} = require('node:fs');
 
 
-let getChangedSchema = async () => {
+
+
+
+async function run() {
+
   let stdout = '';
   const options = {
     listeners: {
@@ -21,24 +25,23 @@ let getChangedSchema = async () => {
 
   const pattern = new RegExp('file-formats/[a-z]{4}/[a-z]+-v[0-9]+\.json$', 'i');
   const lines = stdout.split("\n");
+
   const changedSchema = lines.filter(line => pattern.test(line));
 
-  return changedSchema;
-}
 
-const processFile = async (file) => {
-  const dataNew = readFileSync(`../${file}`, 'utf8');
-  const schemaNew = JSON.parse(dataNew);
-  const source = {type: 'string'};
+  for (const schema of changedSchema) {
 
-  try {
-    await exec.exec(`git checkout remotes/origin/main -- `, [file], { cwd: `../`} );
-  } catch (error) {
-    core.info(`File ${file} is not known to main branch.`);
-    // file is not on main branch, so we continue and compare the file to itself (no harm)
-  }
+    const dataNew = readFileSync(`../${schema}`, 'utf8');
+    const schemaNew = JSON.parse(dataNew);
 
-    const dataOld = readFileSync(`../${file}`, 'utf8');
+    try {
+      await exec.exec(`git checkout remotes/origin/main -- `, [schema], { cwd: `../`} );
+    } catch (error) {
+      core.info(`File ${schema} is not known to main branch.`);
+      // file is not on main branch, so we continue and compare the file to itself (no harm)
+    }
+
+    const dataOld = readFileSync(`../${schema}`, 'utf8');
     const schemaOld = JSON.parse(dataOld);
 
     delete schemaOld['$schema'];
@@ -46,20 +49,12 @@ const processFile = async (file) => {
 
     const result = await jsonSchemaDiff.diffSchemas( {sourceSchema: schemaOld, destinationSchema: schemaNew});
     if (result.removalsFound) {
-      core.error('Something was removed!');
+      core.setFailed('Something was removed');
     }
 
     if (result.additionsFound) {
-      core.error('Something was added!');
-    }
-  }
-
-
-async function run() {
-
-  const changedSchema = await getChangedSchema();
-  for (const schema of changedSchema) {
-    await processFile(schema);
+      core.setFailed('Something was added');
+    };
   }
 }
 
