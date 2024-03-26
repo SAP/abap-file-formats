e3df4420ab0deb667ab96c918c6a9b2b1e668ae4
Albert Mink <albert.mink@sap.com>
2024-03-26 15:57:41 +0100

    Generate JSON Schema for formatVersion != 1  (#604)

--- generate/aff.mjs
+++ generate/aff.mjs
@@ -31,9 +31,10 @@ async function run() {
 
 
   const types = [];
-  for (const f of fs.readdirSync("../file-formats/")) {
+  const directory = '../file-formats'; // Replace with your directory
+  for (const f of fs.readdirSync(directory)) {
     if (f.length === 4) {
-      types.push(f.toUpperCase());
+      types.push(f.toLowerCase());
     }
   }
   types.sort();
@@ -41,27 +42,58 @@ async function run() {
 
   let error = false;
   for (const type of types) {
-    if (type === "ENHO") {
-      core.notice(type + " skipped, https://github.com/SAP/abap-file-formats/issues/409");
+    if (type.toUpperCase() === "ENHO") {
+      core.notice(type.toUpperCase() + " skipped, https://github.com/SAP/abap-file-formats/issues/409");
       continue;
     }
 
-    const result = await abap.Classes["CL_RUN"].run({ object_type: new abap.types.String().set(type) });
-    const filename = "generated" + path.sep + type.toLowerCase() + "-v1.json";
-    const filename_aff = `../file-formats/${type.toLowerCase()}/${type.toLowerCase()}-v1.json`;
-    fs.writeFileSync(filename, result.get());
+    const files = fs.readdirSync(directory + path.sep + type);
+    const jsonFiles = files.filter(file => path.extname(file) === '.json');
+    let objTypeVersNumb = jsonFiles.map((file) => {
+      // Extract the characters before '-v' as namePart and the number after '-v' as versionNumber using regex
+      let match = file.match(/(.*)(?:-v)(\d+)(?=\.json)/);
+
+      if (match) {
+        // Create and return an object with properties 'namePart' and 'versionNumber'
+        let [_, object_type, format_version] = match;
+
+        return { object_type, format_version };
+      }
+
+      return null;
+    });
+
+    for (let aff of objTypeVersNumb) {
+      if (aff) {
+        //core.info(`AFF type: ${aff.object_type}-v${aff.format_version}`);
+        let main_object_type = new abap.types.String().set(type);
+        let sub_object_type = new abap.types.String().set(aff.object_type);
+        const result = await abap.Classes["CL_RUN"].run({ main_object_type, sub_object_type, format_version: aff.format_version });
+        const filename = `generated` + path.sep + aff.object_type + `-v` + aff.format_version + `.json`;
+        const filename_aff = `../file-formats/${type}/${aff.object_type}-v` + aff.format_version + `.json`;
+        fs.writeFileSync(filename, result.get());
 
 
-    const command = `diff ${filename_aff} ${filename}`;
-    const output = child_process.execSync(`${command} || true`);
-    if (output.toString().length > 0) {
-      core.setFailed(type + ": Provided and generated JSON Schema differ")
-      createAnnotations(output.toString(), path.resolve(filename_aff));
-      //core.info(command);
-      //core.info(output.toString());
-    } else {
-      core.notice(type + " success");
+        if (!fs.existsSync(filename_aff)) {
+          core.warning('Failed to access file: ' + filename_aff);
+          continue;
+        }
+
+        const command = `diff ${filename_aff} ${filename}`;
+        const output = child_process.execSync(`${command} || true`);
+        if (output.toString().length > 0) {
+          core.setFailed(aff.object_type + "-v" + aff.format_version + ": Provided and generated JSON Schema differ")
+          createAnnotations(output.toString(), path.resolve(filename_aff));
+          //core.info(command);
+          //core.info(output.toString());
+        } else {
+          core.notice(aff.object_type + "-v" + aff.format_version + " generated successfully");
+        }
+
+      }
     }
+
+
 
   }
 
