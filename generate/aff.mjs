d3573c6133e237441c1b8804f44eb6b88f3bd944
Lars Hvam <larshp@hotmail.com>
2026-01-14 13:07:26 +0100

    cleanup

--- generate/aff.mjs
+++ generate/aff.mjs
@@ -30,16 +30,14 @@ async function run() {
   }
 
 
-  const types = ["doma"];
+  const types = [];
   const directory = '../file-formats'; // Replace with your directory
-  /*
   for (const f of fs.readdirSync(directory)) {
     if (f.length === 4) {
       types.push(f.toLowerCase());
     }
   }
   types.sort();
-  */
 
 
   let error = false;
@@ -51,7 +49,6 @@ async function run() {
 
     const files = fs.readdirSync(directory + path.sep + type);
     const jsonFiles = files.filter(file => path.extname(file) === '.json');
-    console.dir(jsonFiles);
     let objTypeVersNumb = jsonFiles.map((file) => {
       // Extract the characters before '-v' as namePart and the number after '-v' as versionNumber using regex
       let match = file.match(/(.*)(?:-v)(\d+)(?=\.json)/);
@@ -65,7 +62,6 @@ async function run() {
 
       return null;
     });
-    console.dir(objTypeVersNumb);
 
     for (let aff of objTypeVersNumb) {
       if (aff) {
